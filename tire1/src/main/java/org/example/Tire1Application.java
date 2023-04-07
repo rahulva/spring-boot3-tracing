38b4af67d59bd6b01c0e52f4b677c3f3b77e70d3
Rahul V R <rahulv.devlog>
2023-04-07 15:11:32 +0200

    Creating adapter by rewriting the necessary portion of jeager clients - WIP

--- tire1/src/main/java/org/example/Tire1Application.java
+++ tire1/src/main/java/org/example/Tire1Application.java
@@ -1,12 +1,17 @@
 package org.example;
 
+import io.opentracing.contrib.spring.tracer.configuration.TracerAutoConfiguration;
+import io.opentracing.contrib.spring.web.starter.SkipPatternAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@ImportAutoConfiguration({TracerAutoConfiguration.class, SkipPatternAutoConfiguration.class, org.example.tracing.ServerTracingAutoConfiguration.class})
 public class Tire1Application {
     public static void main(String[] args) {
+        System.setProperty("JAEGER_SERVICE_NAME", "order-tire1-boot3");
         SpringApplication.run(Tire1Application.class, args);
     }
 }
--- tire2/src/main/java/org/example/Tire2Application.java
+++ tire2/src/main/java/org/example/Tire2Application.java
@@ -1,11 +1,18 @@
 package org.example;
 
+import io.opentracing.contrib.spring.tracer.configuration.TracerAutoConfiguration;
+import org.example.tracing.ServerTracingAutoConfiguration;
+//import io.opentracing.contrib.spring.web.starter.ServerTracingAutoConfiguration;
+import io.opentracing.contrib.spring.web.starter.SkipPatternAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ImportAutoConfiguration({TracerAutoConfiguration.class, SkipPatternAutoConfiguration.class, ServerTracingAutoConfiguration.class})
 public class Tire2Application {
     public static void main(String[] args) {
+        System.setProperty("JAEGER_SERVICE_NAME", "product-tire2-boot3");
         SpringApplication.run(Tire2Application.class, args);
     }
 }
